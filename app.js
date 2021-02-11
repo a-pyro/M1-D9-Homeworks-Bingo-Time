359de0254106bf01a04c17a8cdba92b2a45c8d0f
ardigermenji <ardi.germenji@gmail.com>
2021-02-11 14:43:42 +0100

    adds cells via js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('Hi there! 🔥');
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('dom loaded');
+
+  //memoria
+
+  //referenze dom
+  const board = document.getElementById('board');
+  const fireBtn = document.getElementById('fireBtn');
+
+  //functions
+  const spitRandomNum = (e) => {
+    console.log('ifire');
+  };
+
+  //listeners
+  fireBtn.addEventListener('click', spitRandomNum);
+
+  //genero le celle
+  for (let i = 0; i < 76; i++) {
+    const cell = document.createElement('div');
+    const num = document.createElement('h3');
+    num.innerText = i + 1;
+    cell.appendChild(num);
+    cell.classList.add('cell');
+    num.classList.add('number');
+    board.insertBefore(cell, fireBtn);
+  }
+});
